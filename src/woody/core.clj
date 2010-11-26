03d13632722e30f8435287d720c775e36f63efe8
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-26 18:44:21 +0530

    wrote a test function to ensure that woodstox can handle large xml files

--- src/woody/core.clj
+++ src/woody/core.clj
@@ -1,3 +1,24 @@
 (ns woody.core
   (:require [clojure.contrib.lazy-xml :as lxml])
-  (:import [com.ctc.wstx.stax WstxInputFactory]))
+  (:import [com.ctc.wstx.stax WstxInputFactory]
+           [javax.xml.stream XMLStreamConstants]))
+
+(def *default-file* "/home/vedang/Work/woody/hn.xml")
+
+(defn main
+  "Things start here"
+  [file]
+  (let [w (WstxInputFactory.)]
+    (.configureForLowMemUsage w)
+    (let [x (.createXMLStreamReader w (java.io.File. file))]
+      (loop [e (.next x)
+             depth 0]
+        (cond
+         (= e XMLStreamConstants/END_DOCUMENT) (do
+						 (println "Done")
+						 (.close x))
+         (= e XMLStreamConstants/START_ELEMENT) (do
+                                                  (println "d: " (inc depth) " start of name: " (.getName x))
+						  (recur (.next x) (inc depth)))
+         (= e XMLStreamConstants/END_ELEMENT) (recur (.next x) (dec  depth))
+	 :else (recur (.next x) depth))))))
