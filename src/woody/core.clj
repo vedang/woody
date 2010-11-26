b620831c69fd28f4ad8489187045178b3391d210
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-27 00:16:01 +0530

    Modifications to get-row to fix bugs and get a map for one entry of HN

--- src/woody/core.clj
+++ src/woody/core.clj
@@ -8,31 +8,34 @@
 (def *wstx-factory* (WstxInputFactory.))
 (.configureForLowMemUsage *wstx-factory*)
 
-(defn get-row-and-push
+(defn get-row
   "Extract all relevant data from one row entry in the file"
   [x]
   (loop [m {}
-	 e (.next x)
-	 d 1]
+         e 1
+         d 0]
     (cond
-     (= d 0) m
      (= e XMLStreamConstants/START_ELEMENT) (recur (merge m (extract-element x))
-						   (.next x)
-						   (inc d))
-     (= e XMLStreamConstants/END_ELEMENT) (recur m
-						 (.next x)
-						 (dec d))
+                                                   (.next x)
+                                                   (inc d))
+     (and (= e XMLStreamConstants/END_ELEMENT) (not (= d 0))) (recur m
+                                                                     (.next x)
+                                                                     (dec d))
+     (= e XMLStreamConstants/END_DOCUMENT) (keywordize-keys m)
+     (= d 0) (keywordize-keys m)
      :else (recur m
-		  (.next x)
-		  (d)))))
+                  (.next x)
+                  d))))
 
 (defn extract-element
   "takes a XMLStreamReader pointing at a START_ELEMENT, and returns
 it as a map with it's text value. Use only on inner elements of a row"
   [x]
-  (let [key (.getLocalName x)]
-    (.next x)
-    (hash-map key (.getText x))))
+  (let [key (.getLocalName x)
+        e (.next x)]
+    (if (= e XMLStreamConstants/CHARACTERS)
+      (hash-map key (.getText x))
+      {key " "})))
 
 (defn main
   "Things start here"
@@ -50,7 +53,7 @@ it as a map with it's text value. Use only on inner elements of a row"
        (= e XMLStreamConstants/START_ELEMENT) (cond
                                                ;; this is a row and we should extract it
                                                ;; and push it to mongo
-                                               (= depth 1) (recur (get-row-and-push x)
+                                               (= depth 1) (recur (get-row x)
                                                                   XMLStreamConstants/END_ELEMENT
                                                                   (inc count)
                                                                   (inc depth))
@@ -83,3 +86,18 @@ it as a map with it's text value. Use only on inner elements of a row"
 ;;   <ParentID />
 ;;  </row>
 ;; </HackerNews>
+
+;; javax.xml.stream.XMLStreamConstants
+;; public static final int      ATTRIBUTE       10
+;; public static final int      CDATA   12
+;; public static final int      CHARACTERS      4
+;; public static final int      COMMENT         5
+;; public static final int      DTD     11
+;; public static final int      END_DOCUMENT    8
+;; public static final int      END_ELEMENT     2
+;; public static final int      ENTITY_DECLARATION      15
+;; public static final int      ENTITY_REFERENCE        9
+;; public static final int      NAMESPACE       13
+;; public static final int      NOTATION_DECLARATION    14
+;; public static final int      PROCESSING_INSTRUCTION  3
+;; public static final int      SPACE   6
