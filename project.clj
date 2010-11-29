f97584af8b9d8ec5544853f002dba196551e537b
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-29 19:34:31 +0530

    Added congomongo to project

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [org.mongodb/mongo-java-driver "2.1"]
-		 [clj-json "0.3.1"]]
+		 [clj-json "0.3.1"]
+		 [congomongo "0.1.3-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
 		     [lein-run "1.0.1-SNAPSHOT"]])
